3379a91e75f6131083d21d663931fe4e12996879
Shreysh Mane <shreysh.mane12@gmail.com>
2024-03-20 15:50:03 +0530

    array methods

---
+++ Day3/array.js
@@ -0,0 +1,72 @@
+// var arr=[1,2,3,"Yash",{a:10,b:20}];
+// console.log(arr[3]);
+// console.log(arr.length);
+
+// Iterate over the given array
+// for (let index = 0; index < arr.length; index++) {
+//     const element = arr[index];
+//     console.log(element);
+// }
+
+// // for of
+// console.log("For of ");
+
+// for(let ele of arr){
+//     console.log(ele);
+// }
+
+// // for in
+// console.log("For in ");
+// for(let ind in arr){
+//     console.log(ind);
+//     console.log(arr[ind]);
+// }
+
+
+// Array Methods
+
+// toString()
+// var arrMe=[1,2,3,4];
+// console.log(arrMe);
+// console.log(arrMe.toString());
+
+// Add/ Remove elements from an array
+// push() / pop()  // Operates on last location
+// unshift() / shift() // Operates on first location
+
+// Add ele at end
+// arrMe.push(10);
+// console.log(arrMe);
+
+// // Add ele at start
+// arrMe.unshift(10);
+// console.log(arrMe);
+
+// arrMe.pop();
+// arrMe.pop();
+// arrMe.shift();
+// arrMe.shift();
+// console.log(arrMe);
+
+// Adding element at a particular index;
+var arrMe=[1,2,3,4];
+// arrMe[2]=10;
+// arrMe.splice(2,0,10,"Yash",11);
+// console.log(arrMe);
+// arrMe.splice()
+
+
+// Removing an element using index;
+// arrMe.splice(2,1);
+// console.log(arrMe);
+
+
+// indexOf
+var index=arrMe.indexOf(3); 
+console.log(index); // 2
+
+
+// sort
+// Used to sort elements in an array.
+
+
